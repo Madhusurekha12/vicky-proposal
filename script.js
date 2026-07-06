ea9b560b2de7bf89306dc127ad8d7edd15657629
Madhusurekha12 <madhusurekha17@gmail.com>
2026-07-06 10:12:31 +0530

    Add interactive question script for proposal

---
+++ script.js
@@ -0,0 +1,33 @@
+let questions=[
+"Vickyluh, will you marry me? 💍",
+"Babe, will you be mine forever? ❤️",
+"Will you grow old with me Vicky Mah? 🥹",
+"Will you hold my hand forever? 🤝",
+"Will you keep choosing me every day? ❤️",
+"Will you let me annoy you forever? 😂",
+"Will you make beautiful memories with me? 📸",
+"Will you always love me? ❤️",
+"Can I be your wife and your forever, Thangoww? 💍❤️"
+];
+
+let index=0;
+
+function nextQuestion(){
+
+if(index<questions.length){
+document.getElementById("question").innerHTML=
+`
+<p>${questions[index]}</p>
+<button onclick="nextQuestion()">Yes ❤️</button>
+`;
+index++;
+}
+else{
+document.getElementById("question").innerHTML=
+`
+<h2>Yayyyyy ❤️</h2>
+<p>I Love You Forever Thangoww 💖</p>
+<h1>💍❤️</h1>
+`;
+}
+}
